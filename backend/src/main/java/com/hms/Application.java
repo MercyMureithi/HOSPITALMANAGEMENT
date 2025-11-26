1dd65a1369049f6873dba5f0b19488969d85b580
MercyMureithi <wanjiraphilip90@gmail.com>
2025-11-26 21:40:06 +0300

    Tidied up the codebase - fixed those annoying lint warnings
    Cleaned up a few things that were bugging me:
    - Removed the unused @EnableMethodSecurity import that was just sitting there
    - Fixed the null safety warnings in DataInitializer by removing the weird casting approach
    - The project now compiles nicely with just some minor deprecation warnings left
    
    Much cleaner now - the code feels more professional and ready for the college presentation!

--- backend/src/main/java/com/hms/Application.java
+++ backend/src/main/java/com/hms/Application.java
@@ -2,7 +2,6 @@ package com.hms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 //@EnableMethodSecurity(prePostEnabled = true) // Disabled for college project demo
